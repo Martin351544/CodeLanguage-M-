c4a6866f670b4e1409cce925a2c0fc2ce2fec7ff
Martin <Martin.weiss2410@gmail.com>
2025-10-04 08:07:15 +0700

    ast added
    use ast explorer on https://astexplorer.net/
    
    deno error fixed

---
+++ MLanguage/M/ast.ts
@@ -0,0 +1,36 @@
+export type NodeType = 
+    | "Program" 
+    | "NumericLiteral" 
+    | "identifier" 
+    | "BinaryExpr" ;
+
+
+
+export interface Stmt{
+    kind: NodeType;
+}
+
+export interface Program extends Stmt{
+    kind: "Program";
+    body: Stmt[];
+}
+
+
+export interface Expr extends Stmt{}
+
+export interface BinaryExpr extends Expr{
+    kind: "BinaryExpr";
+    left: Expr;
+    right: Expr;
+    operator: string;
+}
+
+export interface Identifier extends Expr{
+    kind: "identifier";
+    symbol: string;
+}
+
+export interface NumericLiteral extends Expr{
+    kind: "NumericLiteral";
+    value: number;
+}
---
+++ MLanguage/M/lexer.ts
@@ -0,0 +1,87 @@
+// Let x = 45 + (foo * bar)
+
+export enum TokenType{
+    Number,
+    Identifier,
+    Equals,
+    OpenParen, 
+    CloseParen,
+    BinaryOperator,
+    Let,
+    EOF,
+}
+
+const KEYWORDS: Record<string, TokenType> = {
+    "let": TokenType.Let,
+}
+
+export interface Token{
+    value: string,
+    type: TokenType
+}
+
+function token (value = "", type: TokenType): Token{
+    return { value, type };
+}
+
+function isalpha (str: string){
+    return str.toUpperCase() != str.toLowerCase();
+}
+
+function isint(str: string){
+    const c = str.charCodeAt(0);
+    const bounds = ['0'.charCodeAt(0), '9'.charCodeAt(0)];
+    return(c >= bounds[0] && c <=bounds[1]);
+}
+
+function isskippable(str: string){
+    return str == " " || str == "\n" || str == "\t";
+}
+
+export function tokenize(sourceCode: string): Token[] {
+    const tokens = new Array<Token>();
+    const src = sourceCode.split("");
+
+    while (src.length > 0){
+        if(src[0] == '('){
+            tokens.push(token(src.shift(), TokenType.OpenParen));
+        } else if(src[0] == ')'){
+            tokens.push(token(src.shift(), TokenType.CloseParen));
+        } else if(src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/"){
+            tokens.push(token(src.shift(), TokenType.BinaryOperator));
+        } else if (src[0] == "="){
+            tokens.push(token(src.shift(), TokenType.Equals));
+        } else {
+            if(isint(src[0])){
+                let num = '';
+
+                while (src.length > 0 && isint(src[0])){
+                    num += src.shift();
+                }
+
+                tokens.push(token(num, TokenType.Number));
+            }else if (isalpha(src[0])){
+               let ident = '';
+
+                while (src.length > 0 && isalpha(src[0])){
+                    ident += src.shift();
+                }
+
+                const reserved = KEYWORDS[ident];
+                if (reserved == undefined){
+                    tokens.push(token(ident, TokenType.Identifier)); 
+                }else {
+                    tokens.push(token(ident, reserved)); 
+                }
+                
+            }else if (isskippable(src[0])){
+                src.shift();
+            }else{
+                console.log("unrecognised char found in source: ", src[0])
+                Deno.exit(1);;
+            }
+        }
+    }
+    tokens.push({type: TokenType.EOF, value: "EndOfFile"});
+    return tokens;
+}
